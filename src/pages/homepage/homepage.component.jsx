362d40cd7ab062a5edefa15b6b165cd230f9beba
Bruno Poupado <brunopoupado@gmail.com>
2021-09-14 15:58:48 +0100

    slogan update

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -13,7 +13,7 @@ function Homepage() {
             <div className="row">
                 <div className="col-md-12 text-center">
                     <h1 className="under-construction-title">Welcome to TipstersWebsite</h1>
-                    <p>Let the game begin!s</p>
+                    <p>Let the games begin!</p>
                 </div>
             </div>
             <div className="row">
